03e28f48f7dd1c6d9327046e6b956f754a46d882
exploding-scope <kvalji@outlook.com>
2024-08-17 19:33:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ var URLS = [
 var CACHE_NAME = APP_PREFIX + VERSION
 self.addEventListener('fetch', function (e) {
   console.log('Fetch request : ' + e.request.url);
-  if (e.request.method === 'POST') {
+  if (e.request.method === 'POST3') {
       console.log('Form submitted successfully!');// Handle form submission
       e.respondWith(
         fetch(e.request).then(function (response) {
